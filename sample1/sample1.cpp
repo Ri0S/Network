b9712124289c038067fffa81b0228dc32f1635eb
RioS <aa248424@gmail.com>
2016-04-01 15:15:43 +0900

    2

--- sample1/sample1.cpp
+++ sample1/sample1.cpp
@@ -1,23 +1,69 @@
 #include <string.h>
 #include "pcap.h"
 
-/* prototype of the packet handler */
-typedef struct pkt{
+#define IP_HEADER 0x0800
+#define ARP_HEADER 0x0806
+#define REVERSE_ARP_HEADER 0x0835
+#define SYN 0x02
+#define PUSH 0x08
+#define ACK 0x10
+#define SYN_ACK 0x12
+#define PUSH_ACK 0x18
+#define FIN_ACK 0x11
+#define TCP 0x06
+#define UDP 0x11
+
+typedef struct ether_header{
 	u_char dstMac[6];
 	u_char srcMac[6];
-	u_char Type[2];
-	u_char Protocal;
-	const u_char *data;
-	int dataLen;
-	struct in_addr dstIPaddr;
-	struct in_addr srcIPaddr;
-	char dstAddr[16];
-	char srcAddr[16];
-	int dstPort;
-	int srcPort;
-}pkt;
+	u_short type;
+}ether_header;
+
+/* prototype of the packet handler */
+typedef struct ip_header{
+	u_char ip_leng : 4;
+	u_char ip_version : 4;
+	u_char ip_tos;
+	u_short ip_total_length;
+	u_short ip_id;
+	u_short ip_flags_fo;
+	u_char ip_ttl;
+	u_char ip_protocol;
+	u_short ip_checksum;
+	in_addr src_addr;
+	in_addr dst_addr;
+}ip_header;
+
+typedef struct tcp_header{
+	u_short src_port;
+	u_short dst_port;
+	u_int sequence;
+	u_int acknowledge;
+	u_char ns : 1;
+	u_char reserved_part1 : 3;
+	u_char length : 4;
+	u_char flag;
+	u_short window;
+	u_short checksum;
+	u_short urgent_pointer;
+}tcp_header;
+
+typedef struct udp_header{
+	u_short src_port;
+	u_short dst_port;
+	u_short length;
+	u_short checksum;
+};
+
+typedef struct udpHeader{
+
+}udpHeader;
 void packet_handler(u_char *param, const struct pcap_pkthdr *header, const u_char *pkt_data);
-pkt packet_anal(const u_char *pkt_data);
+void print_ether_header(ether_header *eh);
+void print_ip_header(ip_header *ih);
+void print_tcp_header(tcp_header *th);
+void print_udp_header(udp_header *uh);
+void print_data(const u_char *pkt_data, int length);
 
 int main()
 {
@@ -99,91 +145,74 @@ void packet_handler(u_char *param, const struct pcap_pkthdr *header, const u_cha
 	struct tm ltime;
 	char timestr[16];
 	int i, j, k;
-	pkt pk;
-	time_t local_tv_sec;
+	ether_header *eh;
+	ip_header *ih;
+	tcp_header *th;
+	udp_header *uh;
 
-	/*
-	* unused variables
-	*/
-	(VOID)(param);
-	(VOID)(pkt_data);
-
-	pk = packet_anal(pkt_data);
-
-	printf("Destination MAC: ");
-	for (i = 0; i < 6; i++)
-		printf("%02x ", pk.dstMac[i]);
-	printf("\nSource MAC: ");
-	for (i = 0; i < 6; i++)
-		printf("%02x ", pk.srcMac[i]);
-
-	printf("\nSource IP Address : %s\n", pk.srcAddr);
-	printf("Destination IP : %s\n", pk.dstAddr);
-	
-	if (pk.Protocal == 0x06 || pk.Protocal == 0x11){
-
-		printf("Source Port: %d\n", pk.srcPort);
-		printf("Destination Port: %d\n", pk.dstPort);
-
-		for (i = 0; i < pk.dataLen; i += 16){
-			printf("%05d   ", i);
-			for (j = 0; j < 16; j++){
-				printf("%02x ", pk.data[i + j]);
-				if (j == 7)
-					printf(" ");
-			}
-			printf("    ");
-			for (k = 0; k < 16; k++){
-				if (pk.data[i + k] > 32 && pk.data[i + k] < 126)
-					printf("%c", pk.data[i + k]);
-				else
-					printf(".");
-			}
-			printf("\n");
-		}
+	eh = (ether_header*)pkt_data;
+	pkt_data += sizeof(*eh);
+	print_ether_header(eh);
+	ih = (ip_header*)(pkt_data);
+	pkt_data += sizeof(*ih);
+	print_ip_header(ih);
+	if (ih->ip_protocol == TCP){
+		th = (tcp_header*)(pkt_data);
+		pkt_data += sizeof(*th);
+		print_tcp_header(th);
+		print_data(pkt_data, htons(ih->ip_total_length) - ih->ip_leng*4 - th->length*4);
 	}
-	printf("\n");
-
+	else if (ih->ip_protocol == UDP){
+		uh = (udp_header*)(pkt_data);
+		pkt_data += sizeof(*uh);
+		print_udp_header(uh);
+		print_data(pkt_data, htons((uh->length))-8);
+	}
 }
-pkt packet_anal(const u_char *pkt_data){
-	pkt pk = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-	long temp = 0, temp2 = 0;
-	int it;
-	char st[5];
 
-	for (int i = 0; i < 6; i++){
-		pk.dstMac[i] = pkt_data[i];
-		pk.srcMac[i] = pkt_data[i + 6];
-	}
-	pk.Type[0] = pkt_data[12];
-	pk.Type[1] = pkt_data[13];
+void print_ether_header(ether_header *eh){
+	printf("Destination MAC : ");
+	for (int i = 0; i < 6; i++)
+		printf("%02x ", eh->dstMac[i]);
+	printf("\nSource MAC : ");
+	for (int i = 0; i < 6; i++)
+		printf("%02x ", eh->srcMac[i]);
+	
+	printf("\n");
+}
+void print_ip_header(ip_header *ih){
+	printf("Source IP : %s\n", inet_ntoa(ih->src_addr));
+	printf("Destination IP : %s\n", inet_ntoa(ih->dst_addr));
+}
+void print_tcp_header(tcp_header *th){
+	printf("Source Port : %d\n", htons(th->src_port));
+	printf("Destination Port : %d\n", htons(th->dst_port));
+}
+void print_udp_header(udp_header *uh){
+	printf("Source Port : %d\n", htons(uh->src_port));
+	printf("Destination Port : %d\n", htons(uh->dst_port));
+}
+void print_data(const u_char *pkt_data, int length){
+	int k=0;
+	int l = 0;
+	for (int i = 0; k < length; i += 16){
+		printf("%05d   ", i);
+		for (l = 0; l < 16 && (k + l) < length; l++)
+			printf("%02x ", pkt_data[k + l]);
 
-	pk.Protocal = pkt_data[23];
+		printf("   ");
 
-	for (int i = 4; i > 0; i--){
-		temp += (pkt_data[0x1A + 4 - i] << 8 * (i - 1));
-		it = pkt_data[0x1A + 4 - i];
-		sprintf(st, "%d", it);
-		strcat(pk.srcAddr, st);
-		temp2 += (pkt_data[0x1E + 4 - i] << 8 * (i - 1));
-		it = pkt_data[0x1E + 4 - i];
-		sprintf(st, "%d", it);
-		strcat(pk.dstAddr, st);
-		if (i != 1){
-			strcat(pk.srcAddr, ".");
-			strcat(pk.dstAddr, ".");
+		for (int j = 0; j < 16 - l; j++){
+			printf("   ");
 		}
+
+		for (int j = 0; j < 16 && k < length; j++, k++){
+			if (pkt_data[k] > '!' && pkt_data[k] < '}' + 1)
+				putchar(pkt_data[k]);
+			else
+				putchar('.');
+		}
+		putchar('\n');
 	}
-	pk.srcIPaddr.S_un.S_addr = temp;
-	pk.dstIPaddr.S_un.S_addr = temp2;
-
-
-
-	pk.srcPort = (pkt_data[0x22] << 8) + pkt_data[0x23];
-	pk.dstPort = (pkt_data[0x24] << 8) + pkt_data[0x25];
-
-	pk.data = &pkt_data[0x36];
-	pk.dataLen = (pkt_data[0x10] << 8) + pkt_data[0x11] - 40;
-
-	return pk;
+	putchar('\n');
 }
